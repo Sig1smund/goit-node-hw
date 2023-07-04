b6a88fe0a27981a70d8c517d4ca8f0a6f0780797
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-07-04 23:27:39 +0300

    upd

--- contacts.js
+++ contacts.js
@@ -5,31 +5,67 @@ const contactsPath = path.dirname("./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  return contactsPath;
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    return data;
+  });
 }
 
 function getContactById(contactId) {
-  const contact = contactsPath.filter((contact) => contact.id === contactId);
-  return contact.id === contactId ? contact : null;
+  let contacts;
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    contacts = data;
+    const contact = contacts.filter((contact) => contact.id === contactId);
+    return contact.id === contactId ? contact : null;
+  });
   // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
 }
 
 function removeContact(contactId) {
-  const index = contactsPath.findIndex((item) => item.id === contactId);
-  const erasedContact = contactsPath.splice(index, 1);
-  return erasedContact;
+  let contacts;
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    contacts = JSON.parse(data);
+    const index = contacts.findIndex((item) => item.id === contactId);
+    const erasedContact = contactsPath.splice(index, 1);
+    fs.writeFile(contactsPath, JSON.stringify(erasedContact), (error) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  });
   // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
 }
 
 function addContact(name, email, phone) {
+  let contacts;
   const newContact = {
     id: contactsPath.length + 1,
     name,
     email,
     phone,
   };
-  contactsPath.push(newContact);
-  return newContact;
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    contacts = data;
+    contacts.push(newContact);
+    return newContact;
+  });
+
   // ...твой код. Возвращает объект добавленного контакта.
 }
 
